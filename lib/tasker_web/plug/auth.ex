7c2cdaa5e5d437e7e22a2558bdfffa3ded7d942e
Yurii Skrynnykov <truef1s7@gmail.com>
2021-07-14 13:42:33 +0300

    Add scoped authorization

---
+++ lib/tasker_web/plug/auth.ex
@@ -0,0 +1,49 @@
+defmodule TaskerWeb.Plug.Auth do
+  @moduledoc """
+  This Plug used to authorize users
+  """
+  use TaskerWeb, :controller
+
+  @secret Application.compile_env(:tasker, :token_secret)
+
+  def init(opts), do: opts
+
+  def call(conn, opts) do
+    allowed_user_types = Keyword.get(opts, :allowed_user_types)
+
+    case get_req_header(conn, "authorization") do
+      ["Bearer " <> token] ->
+        authorize(conn, token, allowed_user_types)
+
+      _ ->
+        unauthorized(conn)
+    end
+  end
+
+  defp authorize(conn, value, allowed_user_types) do
+    case Phoenix.Token.verify(TaskerWeb.Endpoint, @secret, value) do
+      {:ok, %{user_type: user_type}} ->
+        if user_type in allowed_user_types do
+          do_authorize(conn, value)
+        else
+          unauthorized(conn)
+        end
+
+      _ ->
+        unauthorized(conn)
+    end
+  end
+
+  defp unauthorized(conn) do
+    conn
+    |> put_status(:unauthorized)
+    |> put_view(TaskerWeb.ErrorView)
+    |> render("401.json")
+    |> halt()
+  end
+
+  defp do_authorize(conn, value) do
+    token = Tasker.get_token_by_value(value)
+    assign(conn, :user_id, token.user_id)
+  end
+end
--- lib/tasker_web/router.ex
+++ lib/tasker_web/router.ex
@@ -1,11 +1,35 @@
 defmodule TaskerWeb.Router do
   use TaskerWeb, :router
 
+  alias TaskerWeb.Plug.Auth
+
+  pipeline :manager_api do
+    plug :accepts, ["json"]
+    plug Auth, allowed_user_types: ["manager"]
+  end
+
+  pipeline :driver_api do
+    plug :accepts, ["json"]
+    plug Auth, allowed_user_types: ["driver"]
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
+    plug Auth, allowed_user_types: ["driver", "manager"]
   end
 
   scope "/api", TaskerWeb do
     pipe_through :api
+    resources "/tasks", TaskController, only: [:show]
+  end
+
+  scope "/api", TaskerWeb do
+    pipe_through :manager_api
+    resources "/tasks", TaskController, only: [:create, :delete]
+  end
+
+  scope "/api", TaskerWeb do
+    pipe_through :driver_api
+    resources "/tasks", TaskController, only: [:index, :update]
   end
 end
